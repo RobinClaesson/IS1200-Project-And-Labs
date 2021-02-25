4663a279502adbf46010edd24959a6bf75541181
Lewi <lewi.l.l@live.se>
2021-02-25 18:16:24 +0100

    -

--- Pong/main.c
+++ Pong/main.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#define PI 3.14159
 
 //structures
 struct Point{
@@ -22,11 +23,20 @@ void resetGame();
 void update();
 void draw();
 
+void display_rectangle(struct Rectangle rect);
+
+int rectBot(struct Rectangle rect);
+int rectRight(struct Rectangle rect);
+struct Point rectCenter(struct Rectangle rect);
+
 int collisionRP(struct Rectangle rect, struct Point point);
 int collisionRR(struct Rectangle rect1, struct Rectangle rect2);
 
 void moveUp(struct Rectangle* rect);
 void moveDown(struct Rectangle* rect);
+
+void setBallAngle(double angle);
+void ballPaddleAngle(struct Rectangle rect);
 
 //Global Variables
 struct Point screenSize;
@@ -39,29 +49,217 @@ struct Rectangle player2;
 
 int main() {
   display_init();
-  clear_buffer();
-  int i;
 
-  for (i = 0; i < 128; i++){
-    display_pixel(i, 0);
-  }
 
-  display_update();
+
+  screenSize = createPoint(128, 32); //128x32 screen size
+  resetGame();
+
+  draw();
+
+  //update();
+	return 0;
+}
 
 void display_rectangle(struct Rectangle rect){
   int i, j;
 
-  for (i = 0; i <= rect.size.x; i++){
-    for (j = 0; j <= rect.size.y; j++){
+  for (i = 0; i < rect.size.x; i++){
+    for (j = 0; j < rect.size.y; j++){
       display_pixel(rect.pos.x + i, rect.pos.y + j);
     }
   }
 }
 
-  /*
-  screenSize = createPoint(128, 32); //128x32 screen size
-  resetGame();
+void update()
+{
+  ball.pos.x += cos(ballAngle);
+  ball.pos.y += sin(ballAngle);
 
-  update();*/
-	return 0;
+
+  //Ball player1 collision
+  if(collisionRR(player1, ball)){
+      ballPaddleAngle(player1);
+      ball.pos.x  = rectRight(player1);
+  }
+
+  //Ball player2 collision
+  else if(collisionRR(player2, ball)){
+      ballPaddleAngle(player2);
+      ball.pos.x = player2.pos.x - ball.size.x;
+  }
+
+  //Ball bot/top collisionRP
+  if(rectBot(ball) >= screenSize.y)
+  {
+    setBallAngle(-ballAngle);
+    ball.pos.y = screenSize.y - ball.size.y;
+  }
+
+  else if(ball.pos.y <= 0)
+  {
+    setBallAngle(-ballAngle);
+    ball.pos.y = 0;
+  }
+
+
+  //Someone scores
+  if(ball.pos.x < 0 || rectRight(ball) > screenSize.x)
+    resetGame();
+
+
+  draw();
+}
+
+
+void draw()
+{
+  clear_buffer();
+
+  display_rectangle(player1);
+  display_rectangle(player2);
+  display_rectangle(ball);
+
+  display_update();
+}
+
+
+void resetGame(){
+  player1 = createRect(3, screenSize.y/2 - 3, 1, 6);
+  player2 = createRect(screenSize.x - 4., screenSize.y/2 - 3, 1, 6);
+
+  ball = createRect(screenSize.x/2 - 1, screenSize.y/2 - 1, 2, 2);
+  ballAngle = PI;
+
+}
+
+struct Point createPoint(int x, int y){
+  struct Point p;
+  p.x = x;
+  p.y = y;
+  return p;
+}
+
+struct Rectangle createRect(int x, int y, int width, int height){
+  struct Rectangle rect;
+  rect.pos = createPoint(x, y);
+  rect.size = createPoint(width, height);
+
+  return rect;
+}
+
+int collisionRP(struct Rectangle rect, struct Point point){
+  if(point.x >= rect.pos.x && point.x < rect.pos.x + rect.size.x &&
+     point.y >= rect.pos.y && point.y < rect.pos.y + rect.size.y)
+      return 1;
+
+  else return 0;
+}
+
+int collisionRR(struct Rectangle rect1, struct Rectangle rect2){
+    //If rect1 is to the right of rect2
+    if(rect1.pos.x > rectRight(rect2))
+      return 0;
+
+    //if rect1 is to the left of rect2
+    else if(rect2.pos.x > rectRight(rect1))
+      return 0;
+
+    //if rect1 is above rect2
+    else if(rectBot(rect1) < rect2.pos.y)
+      return 0;
+
+    //if rect 1 is below rect2
+    else if(rect1.pos.y >  rectBot(rect2))
+      return 0;
+
+    return 1;
+}
+
+void moveUp(struct Rectangle* rect)
+{
+  if(rect->pos.y > 0)
+    rect->pos.y--;
+}
+
+void moveDown(struct Rectangle* rect)
+{
+  if(rect->pos.y + rect->size.y < screenSize.y)
+    rect->pos.y++;
+}
+
+//Returns the y coardnate for the bottom of the rectangle
+int rectBot(struct Rectangle rect)
+{
+  return rect.pos.y + rect.size.y;
+}
+
+//Recturns the x coardinate for the right of the rectangle
+int rectRight(struct Rectangle rect)
+{
+  return rect.pos.x + rect.size.x;
+}
+
+//Returns the center of the rect
+struct Point rectCenter(struct Rectangle rect)
+{
+    struct Point center;
+    center.x = (rect.pos.x + rectRight(rect))/2;
+    center.y = (rect.pos.y + rectBot(rect))/2;
+    return center;
+}
+
+
+void setBallAngle(double angle)
+{
+    ballAngle = angle;
+
+    if(ballAngle > 2*PI)
+      ballAngle -= 2*PI;
+    else if(ballAngle < 0)
+      ballAngle += 2*PI;
+}
+
+void ballPaddleAngle(struct Rectangle player)
+{
+  setBallAngle(PI - ballAngle);
+
+  double dist = rectCenter(player).y - rectCenter(ball).y;
+  double offset = (PI/3)*(dist/player.size.y); //Max offset * percentage distance from middle
+
+  //The offset have different sign for the two players
+  if(ball.pos.x > player.pos.x)
+    offset *= -1;
+
+  setBallAngle(ballAngle + offset);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+double cos (double x){
+  int y;
+
+  y = 1 - (x*x)/2;
+
+  return y;
+}
+
+double sin (double x){
+  int y;
+
+  y = x - (x*x*x)/6;
+
+  return y;
 }
