9266c2b1414c52ed3e721ac1d8a9e3a280287f1e
Lewi <lewi.l.l@live.se>
2021-03-05 15:26:35 +0100

    Revert "--"
    This reverts commit 00e3e64e8db9fef1e1082609b672a5c5cfe001c8.

--- Pong/main.c
+++ Pong/main.c
@@ -724,6 +724,7 @@ void menu_down()
 
 void choose_name(){
   static int i = 0;
+  //name = "aaa";
 
   if (btn1_pressed()){
     if (i >= 2) {
@@ -741,7 +742,7 @@ void choose_name(){
 
   if (btn3_pressed()){
     if (name[i] < 0x7a) {
-      name[i]--;
+      name[i]++;
     } else
       name[i] = 0x61;
   }
